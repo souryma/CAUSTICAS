66ac7348348daca784598eb83cca88c3cd1f18aa
souryma <antoine.martin885@gmail.com>
2024-03-07 17:08:43 +0100

    Added prefabs + updated script

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ProceduralGeneration : MonoBehaviour
 {
+    [SerializeField] private GameObject bigRoomPrefab;
+
+    [SerializeField] private float2 gridSize; 
+
+    private GameObject[,] grid;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                grid[x, y] = bigRoomPrefab;
+            }
+        }
     }
 }
