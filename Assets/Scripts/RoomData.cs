f2709ef648c3d4ffa567fd5dbb3c0d9b1d272bd0
souryma <antoine.martin885@gmail.com>
2024-03-08 15:38:58 +0100

    Added template room prefabs

---
+++ Assets/Scripts/RoomData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Room", fileName = "Room")]
+public class RoomData : ScriptableObject
+{
+    [SerializeField] public GameObject prefab;
+
+    [SerializeField, Range(1, 4)] public int numberOfOpenings;
+
+    private List<RoomData> neighbours;
+    public int id;
+    public int2 coordinates;
+}
