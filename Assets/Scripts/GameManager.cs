405737e7fdcfa1ed76528846d58cad70b81520c0
PierreVandel <pierre.vdl@orange.fr>
2024-04-17 16:23:18 +0200

    Add Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool hasDivingSuit = false;
+    public bool hasKey = false;
+    public bool vaccumIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
