54803d681f2258f9fae1bb690949c33528518e2b
souryma <antoine.martin885@gmail.com>
2024-04-18 09:51:11 +0200

    Added H4MM3R

--- Assets/Scripts/VacuumComponent.cs
+++ Assets/Scripts/VacuumComponent.cs
@@ -10,21 +10,18 @@ public class VacuumComponent : MonoBehaviour
 
     [SerializeField] private MeshRenderer eyeRenderer;
 
+    [SerializeField] private float rotationTime = 0.2f;
+    [SerializeField] private int rotationAngle = 10;
+
     private RaycastHit frontRay;
     private RaycastHit rightRay;
     private RaycastHit leftRay;
 
     private bool isVaccumAngry = false;
     private bool isTurning = false;
+    private bool isColorRed = false;
 
-    private void TweenCompleted()
-    {
-        isTurning = false;
-    }
 
-    private float rotationTime = 0.2f;
-    private int rotationAngle = 10;
-    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
@@ -32,52 +29,57 @@ public class VacuumComponent : MonoBehaviour
 
         if (isVaccumAngry)
         {
+            if (isColorRed == false)
+            {
+                eyeRenderer.material.color = Color.red;
+                eyeRenderer.material.SetColor("_EmissionColor", Color.red);
+                isColorRed = true;
+            }
+
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
 
             transform.LookAt(target.transform);
-
-            eyeRenderer.material.color = Color.red;
-            eyeRenderer.material.SetColor("_EmissionColor", Color.red);
         }
         else
         {
-            if (isTurning == false) 
+            if (isTurning == false)
             {
                 frontRay = DrawSphereCast(transform.forward);
                 rightRay = DrawSphereCast(transform.right);
                 leftRay = DrawSphereCast(-transform.right);
-                
+
                 transform.position = Vector3.MoveTowards(transform.position, frontRay.point, speed);
 
                 //transform.LookAt(frontRay.point);
-                
+
                 if (rightRay.distance > frontRay.distance)
                 {
                     isTurning = true;
-                    transform.DORotate(new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + rotationAngle, transform.localEulerAngles.z), rotationTime).onComplete = TweenCompleted;
+                    transform.DORotate(
+                        new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + rotationAngle,
+                            transform.localEulerAngles.z), rotationTime).onComplete = TweenCompleted;
                 }
                 else if (leftRay.distance > frontRay.distance)
                 {
                     isTurning = true;
-                    transform.DORotate(new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - rotationAngle, transform.localEulerAngles.z), rotationTime).onComplete = TweenCompleted;
+                    transform.DORotate(
+                        new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - rotationAngle,
+                            transform.localEulerAngles.z), rotationTime).onComplete = TweenCompleted;
                 }
             }
-
-            
         }
+    }
+
+    private void TweenCompleted()
+    {
+        isTurning = false;
     }
 
     private RaycastHit DrawSphereCast(Vector3 direction)
     {
         RaycastHit hit;
 
-        var castOrigin = new Vector3(transform.position.x, 1f, transform.position.z);
-
-        if (Physics.SphereCast(castOrigin, 0.7f, direction, out hit))
-        {
-            Debug.DrawRay(castOrigin, transform.forward, Color.red);
-            Debug.DrawLine(castOrigin, hit.point, Color.green);
-        }
+        Physics.SphereCast(new Vector3(transform.position.x, 1f, transform.position.z), 0.7f, direction, out hit);
 
         hit.point = new Vector3(hit.point.x, 0, hit.point.z);
 
