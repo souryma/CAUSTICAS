ad2042d350424da77c2f33ac3926e0890e43b013
souryma <antoine.martin885@gmail.com>
2024-04-24 13:35:09 +0200

    Added ending

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,10 @@
+using System;
+using TMPro;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,7 +12,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
@@ -30,6 +34,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AudioSource _backgroundSource;
     [SerializeField] private AudioClip _underwaterAudioClip;
     [SerializeField] private Rigidbody playerRB;
+    [SerializeField] private GameObject _bubulles;
+    [SerializeField] private GameObject UI;
+    [SerializeField] private Slider slider;
+    [SerializeField] private GameObject endingText;
+    [SerializeField] private FirstPersonMovement playerMovement;
 
     private LensDistortion _lensDistortion;
     private Vignette _vignette;
@@ -40,7 +49,7 @@ public class GameManager : MonoBehaviour
         _postProcessData.profile.TryGet(out _lensDistortion);
         _postProcessData.profile.TryGet(out _vignette);
         _postProcessData.profile.TryGet(out _chromaticAberration);
-        
+
         _lensDistortion.active = false;
         _vignette.active = false;
         _chromaticAberration.active = false;
@@ -52,23 +61,66 @@ public class GameManager : MonoBehaviour
         return Player;
     }
 
+    private bool oxygenSliderActivation = false;
+
+    private Color underWaterColor = new Color(0, 0.3607843f, 0.7176471f);
+
     public void SetUnderWaterColor()
     {
-        _portalCard.material.color = new Color(0, 0.3607843f, 0.7176471f);
+        _portalCard.material.color = underWaterColor;
+        _portalCard.material.SetFloat("_Alpha", 1);
         _backgroundSource.clip = _underwaterAudioClip;
+        _backgroundSource.loop = true;
+        _backgroundSource.Play();
+        _bubulles.SetActive(true);
 
         playerRB.drag = 5;
+
+        oxygenSliderActivation = true;
+    }
+
+    private float _fadeDistance = 40;
+
+    private void Update()
+    {
+        if (oxygenSliderActivation)
+        {
+            slider.value -= 0.06f * Time.deltaTime;
+            float h, s, v;
+            Color.RGBToHSV(underWaterColor, out h, out s, out v);
+            v -= 0.06f * Time.deltaTime;
+            underWaterColor = Color.HSVToRGB(h, s, v);
+            _portalCard.material.SetColor("_Color", underWaterColor);
+            _fadeDistance -= 0.2f;
+            if (_fadeDistance < 0)
+                _fadeDistance = 0;
+            _portalCard.material.SetFloat("_FadeDistance", _fadeDistance);
+
+            if (slider.value == 0)
+            {
+                endingText.SetActive(true);
+                slider.gameObject.SetActive(false);
+                playerMovement.speed = 0;
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     public void GetDivingSuit()
     {
         hasDivingSuit = true;
-        
+
         _lensDistortion.active = true;
         _vignette.active = true;
         _chromaticAberration.active = true;
+
+        UI.SetActive(true);
     }
-    
+
     public void PlayerWaterFillingSound()
     {
         _waterSound.Play();
